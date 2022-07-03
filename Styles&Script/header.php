fd9d9d6aa7e5880b12f0bfc358d0d5cc4ed0edc1
mouad ALLAOUI <96632243+MouadALLAOUI@users.noreply.github.com>
2022-07-03 20:32:45 +0200

    adding new img and ico , working on the lift side of the web

--- Styles&Script/header.php
+++ Styles&Script/header.php
@@ -8,6 +8,55 @@
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
       rel="stylesheet"/>
-    <link rel="stylesheet" href="Styles&Script/myStyle.css">
+    <link rel="stylesheet" href="styles&script/Style/myStyle.css">
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg bg-light fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="media/images/home.ico" alt="home" class="logo">
+          <span>Logo</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                <img src="media/images/news.ico" alt="News" class="logo">
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <img src="media/images/message.ico" alt="message" class="logo">
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="media/images/notif.ico" class="logo" alt="notif">
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">new friends</a></li>
+                <li><a class="dropdown-item" href="#">mouad liked your post</a></li>
+                <li><a class="dropdown-item" href="#">moha wish you a good birth day</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li class="w-100 btn btn-warning" ><a class="dropdown-item" href="#">make all read</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div class="btn-group dropstart">
+            <button type="button" class="btn btn-light" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="media/images/users.ico" class="logo" alt="users">
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">account</a></li>
+              <li><a class="dropdown-item" href="#">friends list</a></li>
+              <li><a class="dropdown-item" href="#">setting</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Disconnect</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
--- index.php
+++ index.php
@@ -3,37 +3,85 @@
   require "Styles&Script/header.php";
 ?>
 
-    <div class="navbar navbar-secondary bg-secondary">
-      <ul class="nav nav-tabs ms-3">
-        <li class="nav-item">
-          <a href="#" class="nav-link active">
-            <img src="" alt="logo">
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <img src="" alt="News">
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <img src="" alt="message">
-          </a>
-        </li>
-        <!-- Example single danger button -->
-        <li class="btn-group">
-          <img src="" class="btn btn-danger dropdown-toggle" alt="notif" data-bs-toggle="dropdown" aria-expanded="false">
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Separated link</a></li>
-          </ul>
-        </li>
+<main class="container-fluid row mt-5 p-5">
+  <div class="col d-flex flex-row-reverse justify-content-center border flex-wrap">
+    <div class="container d-flex flex-row-reverse flex-wrap align-items-center justify-content-center shadow p-3 mb-5 bg-body rounded">
+      <p class="w-100 text-center">my Profile</p>
+      <div class="w-100  text-center"><img src="media/images/users.ico" alt="profile photo"></div>
+      <hr>
+      <ul>
+        <li>devlopment</li>
+        <li>morocco</li>
+        <li>juillet 2022</li>
       </ul>
     </div>
-    
+    <div class="container d-flex flex-row-reverse flex-wrap align-items-center justify-content-center shadow mb-5 rounded list-group bg-body">
+        <a 
+          data-bs-toggle="collapse"
+          href="#collapse1" role="button"
+          aria-expanded="false"
+          aria-controls="collapse1"
+          class="btn btn-outline-dark btn-outline-dark w-100 mb-1 text-start"
+          >
+            <img class="logo" src="media/images/group.ico" alt="groups">
+            <span>My Groups</span>
+        </a>
+        <div class="collapse w-100 mx-1" id="collapse1">
+          <div class="card card-body">
+          Some text ...
+          </div>
+        </div>
+        <a
+          data-bs-toggle="collapse"
+          href="#collapse2"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapse2"
+          class="btn btn-outline-dark btn-outline-dark w-100 mb-1 text-start"
+          >
+          <img class="logo" src="media/images/event.ico" alt="events">
+          <span>My events</span>
+        </a>
+        <div class="collapse w-100 mx-1" id="collapse2">
+          <div class="card card-body">
+          Some Other text ...
+          </div>
+        </div>
+        <a
+          data-bs-toggle="collapse"
+          href="#collapse3"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapse3"
+          class="btn btn-outline-dark btn-outline-dark w-100 mb-1 text-start"
+          >
+          <img class="logo" src="media/images/album.ico" alt="album">
+          <span>My Albums</span>
+        </a>
+        <div class="collapse w-100 mx-1" id="collapse3">
+          <div class="card card-body">
+                  <img src="media/images/image1.jpg" class="myImgSm" alt="image 1">
+                  <img src="media/images/image2.jpg" class="myImgSm" alt="image 2">
+                  <img src="media/images/image3.jpg" class="myImgSm" alt="image 3">
+                  <img src="media/images/image4.jpg" class="myImgSm" alt="image 4">
+                  <img src="media/images/image5.jpg" class="myImgSm" alt="image 5">
+                  <img src="media/images/image6.jpg" class="myImgSm" alt="image 6">
+          </div>
+        </div>
+    </div>
+  </div>
+  <div class="col-7 border"></div>
+  <div class="col border"></div>
+</main>
+
+
+
+
+
+
+
+
+
 <?php
   require "Styles&Script/Footer.php";
 ?>
